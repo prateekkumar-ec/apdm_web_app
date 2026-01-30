4973a0322efac64ee9659c784c30d09fcaa80ffc
Prateek <138556813+prateekkumar-ec@users.noreply.github.com>
2026-01-30 10:44:13 +0530

    fronten env setup.

--- frontend/src/api/backend.ts
+++ frontend/src/api/backend.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import ENV from "../config/env.config";
 
 const backend = axios.create({
-  baseURL: "http://127.0.0.1:8000", // FastAPI backend
+  baseURL: ENV.API_BASE_URL,
 });
 
 export default backend;
---
+++ frontend/src/config/env.config.ts
@@ -0,0 +1,6 @@
+const ENV = {
+    API_BASE_URL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000',
+    // Add more environment variables as needed
+};
+
+export default ENV;
